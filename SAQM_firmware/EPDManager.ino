9f10b8d4e3367f9c5cfcde69bbfcf44715c20d7f
Juan <juanfloresm92@gmail.com>
2024-02-22 20:15:06 +0100

    Implmenting funciton to translate wind degree to string wind direction

--- SAQM_firmware/EPDManager.ino
+++ SAQM_firmware/EPDManager.ino
@@ -16,15 +16,23 @@
 GxEPD2_BW<GxEPD2_290_BS, GxEPD2_290_BS::HEIGHT> display(GxEPD2_290_BS(EPD_CS,  EPD_DC,  EPD_RES, EPD_BUSY));
 
 void EPDInit(bool clearDisplay){
-    display.init(115200,true,50,false);
+    display.init(115200,clearDisplay, clearDisplay ? 10 : 2,true);
     display.setRotation(1);
 
-    if(clearDisplay) display.clearScreen();
+    if(!clearDisplay){
+        //After deep sleep wake up, the first thing that you print will be blurry,
+        //so to avoid on important data, we print a little dot on the corner
+        display.setPartialWindow(0, 0, 1, 1);
+        display.firstPage();
+        display.drawCircle(0,0,1,GxEPD_BLACK);
+        display.nextPage();
+    }
 }
 
 void EPDUpdateDate(struct tm dateInfo, bool updateDate){
     
     display.setPartialWindow(hourDateBox.X, hourDateBox.Y, hourDateBox.Width, hourDateBox.Height);
+    //display.setPartialWindow(0, 0, display.width(), display.height());
     display.setFont(&FreeSansBold24pt7b);
     display.setTextColor(GxEPD_BLACK);
 
@@ -53,7 +61,7 @@ void EPDUpdateDate(struct tm dateInfo, bool updateDate){
     display.nextPage();
 }
 
-void EPDUpdateForecast(struct forecastData data){
+void EPDUpdateForecast(struct currenWeatherData data){
     // Data preparation
     std::string tempForecast = std::to_string(data.temperature) + "C";
     std::string humidityForecast = std::to_string(data.humidity) + "%";
@@ -76,7 +84,7 @@ void EPDUpdateForecast(struct forecastData data){
     display.print(humidityForecast.c_str());
 
     display.setCursor(forecastBox.cursorX + forecastBox.offsetDataWind, forecastBox.cursorY - forecastBox.primaryFontSize);
-    display.print(data.windDir.c_str());
+    display.print(getWindName(data.windDir).c_str());
 
     display.setCursor(forecastBox.cursorX + forecastBox.offsetDataWind, forecastBox.cursorY);
     display.print(windSPeedForecast.c_str());
@@ -96,13 +104,11 @@ void EPDUpdateIndoorMeas(struct BME680readings readings){
     uint16_t originalCursorY = envioromentalBox.cursorY;
 
     display.setPartialWindow(envioromentalBox.X, envioromentalBox.Y, envioromentalBox.Width, envioromentalBox.Height);
-    display.setFont(&FreeSans9pt7b);
-
     display.setTextColor(GxEPD_WHITE);
 
     display.firstPage();
     display.fillRect(envioromentalBox.X, envioromentalBox.Y, envioromentalBox.Width, envioromentalBox.Height, GxEPD_BLACK);
-
+    
     display.setFont(&FreeSansBold9pt7b);
     display.setCursor(envioromentalBox.X, envioromentalBox.cursorY);
     display.print("Indoor");
@@ -143,4 +149,23 @@ void EPDUpdateIndoorMeas(struct BME680readings readings){
     envioromentalBox.cursorY = originalCursorY;
 }
 
-void EPDUpdateHWStatus(){}
+void EPDUpdateHWStatus(){
+
+}
+
+std::string getWindName(int degree) {
+    // Normalize degree
+    degree = degree % 360;
+    if (degree < 0) {
+        degree += 360;
+    }
+
+    const uint16_t degreePerDirection = 360 / windDirection["ENG"].size();
+
+    uint8_t directionIndex = (degree + degreePerDirection / 2) / degreePerDirection;
+    if (directionIndex == windDirection["ENG"].size()) {
+        directionIndex = 0; 
+    }
+
+    return windDirection["ENG"][directionIndex];
+}
